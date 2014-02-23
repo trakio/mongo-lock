f0263c3e87d1ba40fcde568798c49a81d6e13009
Matthew Spence <msaspence@gmail.com>
2014-02-23 18:31:17 +0000

    Added tests for acquired?

--- spec/acquired_spec.rb
+++ spec/acquired_spec.rb
@@ -4,10 +4,13 @@ describe Mongo::Lock do
 
   describe '#acquired?' do
 
+    let(:lock) { Mongo::Lock.new 'my_lock', owner: 'spence', timeout_in: 0.01, frequency: 0.01 }
+
     context "when the lock has been acquired" do
 
       it "returns true" do
-
+        lock.acquire
+        expect(lock.acquired?).to be_true
       end
 
     end
@@ -15,19 +18,19 @@ describe Mongo::Lock do
     context "when the lock hasn't been acquired" do
 
       it "returns false" do
-
+        collection.insert key: 'my_lock', owner: 'tobie', expires_at: 1.minute.from_now
+        lock.acquire
+        expect(lock.acquired?).to be_false
       end
 
     end
 
     context "when the lock is already acquired but by the same owner in a different thread" do
 
-      it "sets this instance as acquired" do
-
-      end
-
       it "returns true" do
-
+        collection.insert key: 'my_lock', owner: 'spence'
+        lock.acquire
+        expect(lock.acquired?).to be_true
       end
 
     end
@@ -35,7 +38,10 @@ describe Mongo::Lock do
     context "when the lock was acquired but has since expired" do
 
       it "returns false" do
-
+        collection.insert key: 'my_lock', owner: 'spence', expires_at: 0.01.seconds.from_now
+        lock.acquire
+        sleep 0.02
+        expect(lock.acquired?).to be_false
       end
 
     end
@@ -43,7 +49,10 @@ describe Mongo::Lock do
     context "when the lock was acquired but has since been released" do
 
       it "returns false" do
-
+        collection.insert key: 'my_lock', owner: 'tobie', expires_at: 1.minute.ago
+        lock.acquire
+        lock.release
+        expect(lock.acquired?).to be_false
       end
 
     end
