fce274df0c5419cf03691d3b9013ffd6dc41ec25
Matthew Spence <msaspence@gmail.com>
2014-02-24 22:44:43 +0000

    Made some test times more leaniant

--- spec/acquired_spec.rb
+++ spec/acquired_spec.rb
@@ -27,8 +27,9 @@ describe Mongo::Lock do
 
     context "when the lock was acquired but has since expired" do
 
+      let(:lock) { Mongo::Lock.new 'my_lock', owner: 'spence', timeout_in: 0.01, frequency: 0.01, expire_in: 0.1 }
+
       it "returns false" do
-        collection.insert key: 'my_lock', owner: 'spence', expires_at: 0.1.seconds.from_now
         lock.acquire
         sleep 0.2
         expect(lock.acquired?).to be_false
